0a1150ca0bff19ea4f1b8356bc1a62311d7228ae
gjotuur <v.chekhovskyi@proton.me>
2025-11-24 00:35:04 +0200

    Initial

---
+++ rabine_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+
+    return 0;
+}
